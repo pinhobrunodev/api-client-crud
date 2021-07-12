18b034e333800955ff940a822ef91d414aa93c50
pinhobrunodev <iaishowcs@gmail.com>
2021-07-12 19:16:03 -0300

    project created

---
+++ desafio-modulo-1-dsclient/src/main/java/com/devsuperior/desafiodsclientmodulo1/DesafioDsclientModulo1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodsclientmodulo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDsclientModulo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDsclientModulo1Application.class, args);
+	}
+
+}
---
+++ desafio-modulo-1-dsclient/src/test/java/com/devsuperior/desafiodsclientmodulo1/DesafioDsclientModulo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodsclientmodulo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDsclientModulo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
